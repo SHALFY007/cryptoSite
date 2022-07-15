45569656917757d8e118c9d5caea9dabb6d82e36
vladislav <vl_evlashin@mail.ru>
2022-07-15 14:41:16 +0300

    Внес в правки в панель навигации

---
+++ index.js
@@ -0,0 +1,7 @@
+/*const dropdown = document.querySelector('.dropdown-toggle');
+dropdown.addEventListener('mouseover', () => {
+    dropdown.classList.remove('dropdown-toggle');
+});
+dropdown.addEventListener('mouseout', () => {
+    dropdown.classList.add('dropdown-toggle');
+});*/
